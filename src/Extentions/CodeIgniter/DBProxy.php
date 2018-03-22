ba7f301f7f581d946d31eebd465b3034b4592bee
Eslam Salem <netcodez@gmail.com>
2018-03-22 12:30:15 +0200

    prepare codeigniter Extensions

--- src/Extentions/CodeIgniter/DBProxy.php
+++ src/Extentions/CodeIgniter/DBProxy.php
@@ -11,16 +11,14 @@ class DBProxy
         $this->db = $db;
         $this->guard = $guard;
     }
-    
+
     public function __call($name, $parameters)
     {
         if (count($parameters) > 0) {
-            $query = new stdClass;
-            $query->sql = $parameters[0];
-            $query->bindings = (isset($parameters[1]))? $parameters[1] : [];
-            $this->guard->attachQuery($query);
+            $bindings = (isset($parameters[1]))? $parameters[1] : [];
+            $this->guard->events->trigger('db.query', [$parameters[0],$bindings]);
         }
-        
+
         return call_user_func_array([$this->db,$name], $parameters);
     }
 }
