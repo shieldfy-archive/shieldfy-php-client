bd79db758803b3efaedd3c145876e440f3ad9b5f
Karim Mohamed <komicho1996@gmail.com>
2018-07-11 20:05:33 +0200

    Symfony: stop Shieldfy guard at cli

--- src/Extensions/Symfony/ShieldfyBundle.php
+++ src/Extensions/Symfony/ShieldfyBundle.php
@@ -13,6 +13,9 @@ class ShieldfyBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
 
     public function runGuard()
     {
+        if (php_sapi_name() === "cli") {
+            return;
+        }
         $shieldfy = Guard::init([
             'app_key' => getenv('SHIELDFY_APP_KEY'),
             'app_secret' => getenv('SHIELDFY_APP_SECRET')
