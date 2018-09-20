19cc74b71eb538d4e6e3b80a5dd58fa551f38458
Karim Mohamed <komicho1996@gmail.com>
2018-09-20 14:13:45 +0200

    return status and message in header to client

--- src/Verified/CheckInstall.php
+++ src/Verified/CheckInstall.php
@@ -8,6 +8,7 @@ class CheckInstall
 {
     public function __construct(Config $config, $collectors)
     {
+        header('X-shieldfy-verification: install');
         $this->config = $config;
         $this->collectors = $collectors;
         $this->notification = new Notification;
@@ -23,12 +24,13 @@ class CheckInstall
 
         $hash = $this->collectors['request']->get['hash'];
         $appHash = hash_hmac('sha256', $this->config['app_secret'], $this->config['app_key']);
-
         // check of keys
         if ($hash !== $appHash) {
             $this->notification->error([
                 'message' => 'There is an error in the installation keys',
             ]);
+            header('X-shieldfy-verification-status: error');
+            header('X-shieldfy-verification-message: There is an error in the installation keys');
             return;
         }
 
@@ -37,6 +39,8 @@ class CheckInstall
         if (!$status) {
             $notificationType = 'error';
         }
+        header('X-shieldfy-verification-status: ' . $notificationType);
+        header('X-shieldfy-verification-message: ' . $message);
         $this->notification->$notificationType([
             'message' => $message
         ]);
